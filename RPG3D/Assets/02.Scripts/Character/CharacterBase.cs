661731145ab454068947ef0ae8467b4c68b54206
sin yo han <mjwsdw12@naver.com>
2023-01-26 20:45:21 +0900

    RPG3D - BehaviorTree, IDamageable
    BehaviorTree
    - Implemented & Tested BehaviorTree in Enemy
    
    IDamageable
    - Now CharacterBase inherits IDamageable so have hp, hp notifications, being damaged

--- RPG3D/Assets/02.Scripts/Character/CharacterBase.cs
+++ RPG3D/Assets/02.Scripts/Character/CharacterBase.cs
@@ -1,21 +1,60 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class CharacterBase : MonoBehaviour
+public abstract class CharacterBase : MonoBehaviour, IDamageable
 {
     protected CharacterStateMachine machine;
+
+    public event Action<float> OnHPChanged;
+    public event Action OnHPMin;
+    public event Action OnHPMax;
+        
+    public float hp
+    {
+        get => _hp;
+        set
+        {
+            if (value < hpMin) value = hpMin;
+            else if (value > hpMax) value = hpMax;
+
+            _hp = value;
+
+            if (_hp <= hpMin) OnHPMin?.Invoke();
+            else if (_hp >= hpMax) OnHPMax?.Invoke();
+            OnHPChanged?.Invoke(_hp);
+        }
+    }
+        
+    public float hpMin => _hpMin;
+    
+    public float hpMax => _hpMax;
+
+    private float _hp;
+    [SerializeField] private float _hpMin = 0.0f;
+    [SerializeField] private float _hpMax = 100.0f;
 
     protected abstract CharacterStateMachine InitMachine();
     protected abstract void UpdateMachine();
 
-    private void Awake()
-    {
+    protected virtual void Awake()
+    {        
         machine = InitMachine();
     }
 
-    private void Update()
+    protected virtual void Start()
+    {
+        hp = hpMax;
+    }
+
+    protected virtual void Update()
     {
         UpdateMachine();
+    }
+
+    public void Damage(float amount)
+    {
+        hp -= amount;
     }
 }
--- RPG3D/Assets/02.Scripts/Character/CharacterPlayer.cs
+++ RPG3D/Assets/02.Scripts/Character/CharacterPlayer.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.Linq;
 
 public class CharacterPlayer : CharacterBase
 {
+    public int STR = 10;
+
+    [SerializeField] private TargetCaster _targetCasterLeftHand;
+    [SerializeField] private TargetCaster _targetCasterRightHand;
+    private List<GameObject> _targetsCastedBuffer = new List<GameObject>();
     protected override CharacterStateMachine InitMachine()
     {
         return new PlayerStateMachine(gameObject);
@@ -17,4 +24,37 @@ public class CharacterPlayer : CharacterBase
     public void FootL() { }
     public void FootR() { }
     public void Land() { }
+
+    public void ClearTargetCasters()
+    {
+        _targetCasterLeftHand.Clear();
+        _targetCasterRightHand.Clear();
+    }
+
+    public IEnumerable<GameObject> GetAllTargetsCasted()
+    {
+        _targetsCastedBuffer.Clear();
+        _targetsCastedBuffer.AddRange(_targetCasterLeftHand.GetTargets());
+        _targetsCastedBuffer.AddRange(_targetCasterRightHand.GetTargets());
+        return _targetsCastedBuffer;
+    }
+
+    public void LeftHandTargetCasterOn(int on) { _targetCasterLeftHand.on = Convert.ToBoolean(on); }
+    public void RightHandTargetCasterOn(int on) { _targetCasterRightHand.on = Convert.ToBoolean(on); }
+
+    public void DamageToLeftHandTargetsCasted()
+    {
+        foreach (IDamageable target in _targetCasterLeftHand.GetTargets().Select(target => target.GetComponent<IDamageable>()))
+        {
+            target.Damage(STR);
+        }
+    }
+
+    public void DamageToRightHandTargetsCasted()
+    {
+        foreach (IDamageable target in _targetCasterRightHand.GetTargets().Select(target => target.GetComponent<IDamageable>()))
+        {
+            target.Damage(STR);
+        }
+    }
 }
